c8ee31cdbd344565d17bb77ec8868e480b998b9f
yun <2757683956@qq.com>
2025-12-18 22:22:18 +0800

    V3.0.2

--- res/js/tool_unit.js
+++ res/js/tool_unit.js
@@ -4,13 +4,20 @@ function sendIntent(intent, payload = {}) {
 }
 
 // 从路径里取文件名用于展示
+// function titleFromPath(p) {
+//   if (!p) return "未知标题";
+//   const parts = p.split(/[\\/]/);
+//   return parts[parts.length - 1] || p;
+// }
 function titleFromPath(p) {
   if (!p) return "未知标题";
   const parts = p.split(/[\\/]/);
-  return parts[parts.length - 1] || p;
+  const name = parts[parts.length - 1] || p;
+
+  // 去掉最后一个扩展名：xxx.mp3 -> xxx
+  return name.replace(/\.[^./\\]+$/, "");
 }
 
 // 使用 module.exports 暴露函数给其他文件
 window.sendIntent = sendIntent;
 window.titleFromPath = titleFromPath;
-
